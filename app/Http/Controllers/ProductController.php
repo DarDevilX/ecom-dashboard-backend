51f5a589e695ce614c1bd065e1438cbab64571b3
DarDevilX <SamLirsuy5542>
2021-12-25 11:00:37 +0700

    Part 7 Backend

---
+++ app/Http/Controllers/ProductController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\models\Product;
+
+class ProductController extends Controller
+{
+    function addProduct(request $req){
+        $product = new Product;
+        $product->name = $req->input('name');
+        $product->price = $req->input('price');
+        $product->description = $req->input('description');
+        $product->file_path = $req->file('file')->store('products');
+        $product->save();
+        if($product){
+            return ['status'=>true];
+        }else{
+            return ['status'=>false];
+        }
+        return $product;
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -3,7 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UserController;
-
+use App\Http\Controllers\ProductController;
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -20,3 +20,4 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 });
 Route::post('register',[UserController::class,'register']);
 Route::post('login',[UserController::class,'login']);
+Route::post('addProduct',[ProductController::class,'addProduct']);
